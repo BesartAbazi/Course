56a4e0a220bc444fa20b0208809724317bd17c69
BesartAbazi <besart45@hotmail.com>
2023-11-20 23:00:25 +0100

    Updates

--- Node_stdout_stdin.js
+++ Node_stdout_stdin.js
@@ -16,26 +16,6 @@ const testNumber = (input) => {
     }
 }
 
-module.exports.testNumber = (input) => {
-  if (input === 'quit') {
-    process.stdout.write('Ok. Bye!\n')
-    process.exit();
-  }
-  if (!numbers.includes(input)) {
-    process.stdout.write('Choose a number from 1 through 10!\nIs the number ... ')
-  } else if (input === secretValue) {
-    process.stdout.write('Woah you got it! Are you psychic? See you later!\n')
-    process.exit();
-  } else {
-    process.stdout.write("Nope. Guess again!\nIs the number ... ");
-  }
-}
-
-
-
-
-
-
 
 process.stdout.write("I'm thinking of a number from 1 through 10. What do you think it is? \n(Write \"quit\" to give up.)\n\nIs the number ... ");
 
