437c0e4687efc60e389aa08dad54b4a0944cddb3
BesartAbazi <besart45@hotmail.com>
2024-01-09 13:08:28 +0100

    2024-01-09

--- Express_Route_Parameters1.js
+++ Express_Route_Parameters1.js
@@ -2,6 +2,9 @@
     router.param()
     When a specific parameter is present in a route, we can write a function that will perform the necessary lookup and attach it to the req object in subsequent middleware that is run.
     -> See app.param()
+
+    router.param is a powerful tool that we can use to keep our code from repeating core functionality through routes. 
+    This is a pattern we want to frequently follow: identify multiple pieces of code that accomplish the same goal, put it into a single component, let that component do that thing (and update it when we want the thing it does to change — in a single place).
 */
 
 const express = require('express');
